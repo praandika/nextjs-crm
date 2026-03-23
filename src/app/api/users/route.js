4dbe2f67bff31a51438b958af8945c74e154657c
praandika <praandikayoga@gmail.com>
2026-03-23 21:45:06 +0800

    Create POST API

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,9 +1,23 @@
-export async function GET() {
-    const users = [
+let users = [
         { id: 1, name: "Andika", role: "Data Analyst" },
         { id: 2, name: "Desak", role: "Frontend Developer" },
         { id: 3, name: "Inten", role: "Data Scientist" }
     ]
 
+export async function GET() {
     return Response.json(users)
+}
+
+export async function POST(request) {
+    const body = await request.json()
+
+    const newUser = {
+        id: users.length + 1,
+        name: body.name,
+        role: body.role,
+    }
+
+    users.push(newUser)
+
+    return Response.json(newUser)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,54 @@ import { useEffect, useState } from "react"
 
 export default function Home(){
   const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [name, setName] = useState("")
+  const [role, setRole] = useState("")
+
+  const fetchUsers = async () => {
+    const res = await fetch("api/users")
+    const data = await res.json()
+    setUsers(data)
+  }
 
   useEffect(() => {
-    fetch("/api/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data)
-        setLoading(false)
-      })
+    fetchUsers()
   }, [])
 
-  if (loading) return <h2>Loading...</h2>
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await fetch("api/users", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({name, role}),
+    })
+
+    setName("")
+    setRole("")
+    fetchUsers()
+  }
 
   return (
     <div style={{padding: "40px"}}>
       <h1>Next.js Fullstack CRM</h1>
+
+      <form onSubmit={handleSubmit} style={{marginBottom: "20px"}}>
+        <input
+          placeholder="Input Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          placeholder="Input Role"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        />
+
+        <button style={{marginLeft: "10px"}}>
+          Tambah
+        </button>
+      </form>
 
       {users.map((user) => (
         <div key={user.id} style={{marginTop: "15px"}}>
